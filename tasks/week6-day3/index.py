d46d5092a8b1b2bf9e5ad3270061d51541364600
Punhan Huseynov <husepunhan@gmail.com>
2021-06-10 23:15:23 +0400

    tasklarin bei hisselri olundu

---
+++ tasks/week6-day3/index.py
@@ -0,0 +1,18 @@
+#1
+def sumlist():
+    lis=[4,56,78,90]
+    result=0
+    for i in lis:
+        result+=i
+
+    print(result)
+
+
+def multplylist():
+    lis=[8, 2, 3, -1, 7]
+    result=1
+    for i in lis:
+        result*=i
+    print(result)
+
+multplylist()
