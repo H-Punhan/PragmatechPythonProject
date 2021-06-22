ac86bc82cb2831d27793eb6d563a1533416c3ef8
Punhan Huseynov <husepunhan@gmail.com>
2021-06-22 21:28:30 +0400

    task elave olundu

--- tasks/exponentation/index.py
+++
@@ -1,9 +0,0 @@
-def topNum(num,top=1):
-    staticNum=num
-    for i in range(0,top):
-        num=num*staticNum
-    return num
-
-print(topNum(5,2))
-    
-
--- tasks/message/app/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User
-
-class Messages(models.Model):
-    sender=models.ForeignKey(User,on_delete=models.CASCADE,null=True,related_name='sender_id')
-    owner=models.ForeignKey(User,on_delete=models.CASCADE,null=True,related_name='owner_id')
-    message=models.TextField()
-
-# Create your models here.
