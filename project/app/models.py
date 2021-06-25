b264fa36d1015f3a802dcdf6ea6789b3f8f8293a
Punhan Huseynov <husepunhan@gmail.com>
2021-06-25 21:32:10 +0400

    Model quruldu

--- project/app/models.py
+++ project/app/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+
+# Blog ucun olan model
+class Blogs(models.Model):
+    title=models.CharField(max_length=50,blank=True)
+    content=models.TextField(blank=True)
+    image=models.FileField(upload_to='static/assets/img')
+    postdate=models.DateField(auto_now=True)
+    author=models.ForeignKey(User,blank=True,on_delete=models.CASCADE)
+
+# Tag modeli
+class Tags(models.Model):
+    name=models.CharField(max_length=25,blank=True)
+
+# Blog ucun olan taglar 
+class Blogtags(models.Model):
+    blog=models.ForeignKey(Blogs,on_delete=models.CASCADE,related_name='blog_id',blank=True)
+    tag=models.ForeignKey(Tags,on_delete=models.CASCADE,related_name='tag_id',blank=True)
+    
+# Commentler
+class Blogcomments(models.Model):
+    name=models.CharField(max_length=25,blank=True)
+    message=models.TextField(blank=True)
+    email=models.EmailField(blank=True)
+    blog=models.ForeignKey(Blogs,blank=True,on_delete=models.CASCADE,related_name='comment_blog_id')
+
